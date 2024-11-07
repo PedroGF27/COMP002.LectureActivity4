874279449928dcaffdadc813173d791f3d06384b
Pedro Garcia <coolcatdopedog@gmail.com>
2024-11-06 18:41:24 -0800

    finished second problem

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let Numbers = [5, 4, 3, 2, 1]
+
+let Numbers = [1, 2, 3, 4, 5];
 
 console.log(Numbers[0]);
 
 console.log(Numbers[4]);
 
 for (let i=0; i < Numbers.length; i++) {
-    
+    console.log(Numbers[i]);
 }
+
+Numbers.push(6, 7, 8, 9, 10);
+
+console.log(Numbers)
