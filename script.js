c38a3aacf8cb058381693ef230c2599ed65e5ac2
Pedro Garcia <coolcatdopedog@gmail.com>
2024-11-06 19:04:23 -0800

    Finished last problem

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 let Numbers = [1, 2, 3, 4, 5];
 
-console.log(Numbers[0]);
+console.log(Numbers[0]);// 1
 
-console.log(Numbers[4]);
+console.log(Numbers[4]);// 5
 
 for (let i=0; i < Numbers.length; i++) {
     console.log(Numbers[i]);
@@ -19,3 +19,6 @@ console.log(Numbers.pop());
 console.log(Numbers.pop());
 
 console.log(Numbers) // [1, 2, 3, 4, 5, 6, 7]
+
+let myString = "Hello World!"
+console.log(myString.length)
